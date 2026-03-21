d25245b4924c0a9607f2e34bca74280409ee9030
BernardoWE <bernardo.augusto8022@gmail.com>
2026-03-21 09:25:24 -0300

    adcionado script para o butao

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 
 const btnSubmit = document.querySelector('.btn')
+const ticket = document.querySelector('.main-ticket')
+const form = document.querySelector('.main-form')
 console.log(btnSubmit)
+
+btnSubmit.addEventListener('click', (event) =>{
+    event.preventDefault()
+    form.classList.add('card-none')
+    ticket.classList.remove('card-none')
+})
