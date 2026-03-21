abb1e0f6a01c707f70949e4f1ac712a3e9c08c7d
BernardoWE <bernardo.augusto8022@gmail.com>
2026-03-21 10:00:06 -0300

    ajustado o HTML para exibir nome corretamente nos ingressos gerados; atualizado o JavaScript para manipular o evento de envio do formulário

--- script.js
+++ script.js
@@ -2,10 +2,17 @@
 const btnSubmit = document.querySelector('.btn')
 const ticket = document.querySelector('.main-ticket')
 const form = document.querySelector('.main-form')
-console.log(btnSubmit)
+const fullName = document.querySelector('.full-name')
+console.log(fullName)
 
-btnSubmit.addEventListener('click', (event) =>{
+
+form.addEventListener('submit', (event) =>{
     event.preventDefault()
+
     form.classList.add('card-none')
     ticket.classList.remove('card-none')
+    const inputName = document.getElementById('nome')
+    console.log(inputName.value)
+    fullName.innerHTML = inputName.value
+    
 })
