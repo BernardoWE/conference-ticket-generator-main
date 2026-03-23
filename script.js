a5cdf85aecdcb90842f1af30f152da05f9c50576
BernardoWE <bernardo.augusto8022@gmail.com>
2026-03-23 20:36:55 -0300

    continuando a criacao do ticket

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 const btnSubmit = document.querySelector('.btn')
 const ticket = document.querySelector('.main-ticket')
 const form = document.querySelector('.main-form')
-const fullName = document.querySelector('.full-name')
+const fullName = document.querySelectorAll('.full-name')
 const emailAdress = document.querySelector('.email-adress')
+const githubUsername = document.querySelector('.user-github')
 
 
 form.addEventListener('submit', (event) =>{
@@ -13,6 +14,10 @@ form.addEventListener('submit', (event) =>{
     ticket.classList.remove('card-none')
     const inputName = document.getElementById('nome')
     const inputEmail = document.getElementById('email')
-    fullName.innerHTML = inputName.value
+    const inputGithub = document.getElementById('gitName')
+    fullName.forEach(name => {
+        name.innerHTML = inputName.value
+    })
     emailAdress.innerHTML = inputEmail.value
+    githubUsername.innerHTML = inputGithub.value
 })
