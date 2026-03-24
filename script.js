34c07f3fe63597a0510ddcfe98dd1ca656756e48
BernardoWE <bernardo.augusto8022@gmail.com>
2026-03-24 20:53:36 -0300

    TERMINADO O TICKET

--- script.js
+++ script.js
@@ -2,10 +2,28 @@
 const btnSubmit = document.querySelector('.btn')
 const ticket = document.querySelector('.main-ticket')
 const form = document.querySelector('.main-form')
-const fullName = document.querySelectorAll('.full-name')
-const emailAdress = document.querySelector('.email-adress')
-const githubUsername = document.querySelector('.user-github')
+// const fullName = document.querySelector('.full-name')
+// const ticketName = document.querySelector('.ticket-name')
+// const emailAdress = document.querySelector('.email-adress')
+// const githubUsername = document.querySelector('.user-github')
 
+
+/* form.addEventListener('submit', (event) =>{
+    event.preventDefault()
+
+    form.classList.add('card-none')
+    ticket.classList.remove('card-none')
+    const inputName = document.getElementById('nome')
+    const inputEmail = document.getElementById('email')
+    const inputGithub = document.getElementById('gitName')
+    // fullName.forEach(name => {
+    //     name.innerHTML = inputName.value
+    // })
+    fullName.innerHTML = inputName.value
+    ticketName.innerHTML = inputName.value
+    emailAdress.innerHTML = inputEmail.value
+    githubUsername.textContent = inputGithub.value
+}) */
 
 form.addEventListener('submit', (event) =>{
     event.preventDefault()
@@ -15,9 +33,11 @@ form.addEventListener('submit', (event) =>{
     const inputName = document.getElementById('nome')
     const inputEmail = document.getElementById('email')
     const inputGithub = document.getElementById('gitName')
-    fullName.forEach(name => {
-        name.innerHTML = inputName.value
-    })
-    emailAdress.innerHTML = inputEmail.value
-    githubUsername.innerHTML = inputGithub.value
+    // fullName.forEach(name => {
+    //     name.innerHTML = inputName.value
+    // })
+    document.querySelector('.full-name').innerHTML = inputName.value
+    document.querySelector('.ticket-name').innerHTML = inputName.value
+    document.querySelector('.email-adress').innerHTML = inputEmail.value
+    document.querySelector('.user-github').textContent = inputGithub.value
 })
