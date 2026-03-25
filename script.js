a9a84f525565499ea28c464e95e24b4ca9decca5
BernardoWE <bernardo.augusto8022@gmail.com>
2026-03-24 21:40:25 -0300

    Adicionado mensagens de erro nos inputs

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ const form = document.querySelector('.main-form')
 form.addEventListener('submit', (event) =>{
     event.preventDefault()
 
-    form.classList.add('card-none')
-    ticket.classList.remove('card-none')
     const inputName = document.getElementById('nome')
     const inputEmail = document.getElementById('email')
     const inputGithub = document.getElementById('gitName')
@@ -18,4 +16,33 @@ form.addEventListener('submit', (event) =>{
     document.querySelector('.ticket-name').innerHTML = inputName.value
     document.querySelector('.email-adress').innerHTML = inputEmail.value
     document.querySelector('.user-github').textContent = inputGithub.value
+
+    let hasError = false
+    const erroName = document.querySelector('.erroName')
+    if(inputName.value === ''){
+        erroName.innerHTML = 'Please enter your name'
+        hasError = true
+    } else {
+        erroName.innerHTML = ''
+    }
+    const erroEmail = document.querySelector('.erroEmail')
+    if(inputEmail.value === ''){
+        erroEmail.innerHTML = 'Please enter your email'
+        hasError = true
+    } else {
+        erroEmail.innerHTML = ''
+    }
+    const erroGithub = document.querySelector('.erroGithub')
+    if(inputGithub.value === ''){
+        erroGithub.innerHTML = 'Please enter your GitHub username'
+        hasError = true
+    } else {
+        erroGithub.innerHTML = ''
+    }
+
+    if(!hasError){
+        form.classList.add('card-none')
+        ticket.classList.remove('card-none')
+    }
+
 })
